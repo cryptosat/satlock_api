970575943cb92d8d2e1fab79564be860d0c0c75f
Yan Michalevsky <yan@cryptosat.io>
2023-09-23 23:00:20 +0000

    store_backup_key working

--- serve.py
+++ serve.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, abort, request, jsonify
 from flask_cors import CORS
 from typing import List, Any
 import hashlib
@@ -44,22 +44,73 @@ def store_backup_key():
 @app.route('/guardianapprove', methods=['POST'])
 def guardian_approve():
     data = request.json
-    api_key = request.headers.get('api_key')
+    api_key = request.headers.get('Authorization')
 
-    # Manually calling the verify function
-    # your implementation here...
+    old_loser_address = data['old_loser_address']
 
-    # Implement your existing logic here...
+    # Verify the API key for the provided address
+    entry = KEYS_AND_GUARDIANS.get(old_loser_address)
+    if not entry or api_key not in entry['guardians']:
+        return {"status": "Not authenticated"}, 401
 
-    return jsonify({"status": "Approved"})
+    # Combine and hash the input parameters
+    combined_data = data['guardian_public_key'] + data['new_loser_address'] + data['old_loser_address']
+    data_hash = hashlib.sha256(combined_data.encode()).digest()
+
+    # Load the guardian's provided public key
+    public_key = serialization.load_pem_public_key(data['guardian_public_key'].encode(), backend=default_backend())
+
+    # Verify the signature
+    try:
+        public_key.verify(
+            bytes.fromhex(data['signed_hash']),
+            data_hash,
+            padding.PKCS1v15(),
+            hashes.SHA256()
+        )
+    except:
+        return {"status": "Signature verification failed"}, 400
+
+    # Assuming the signature is valid, do further processing here, for example:
+    entry['approvals'] += 1
+
+    return {"status": "Approved"}
 
 @app.route('/restorekey', methods=['POST'])
 def restore_key():
-    old_user_address = request.json['old_user_address']
+    GUARDIAN_THRESHOLD = 3
+    
+    # Replace this with how you're getting the old_user_address
+    old_user_address = request.json
+    
+    entry = KEYS_AND_GUARDIANS.get(old_user_address)
+    
+    if not entry or entry["approvals"] < GUARDIAN_THRESHOLD:
+        abort(400, "Insufficient approvals or address not found")
 
-    # Implement your existing logic here...
+    # Decrypt the backup key
+    decrypted_key = PRIVATE_KEY.decrypt(
+        entry["enc_backup_key"].encode(),
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    
+    # Encrypt the decrypted key with the provided RSA public key (new_address)
+    new_address_public_key = serialization.load_pem_public_key(entry["new_address"].encode(), 
+                                                               backend=default_backend())
+    # encrypted_key = new_address_public_key.encrypt(
+    #     decrypted_key,
+    #     padding.OAEP(
+    #         mgf=padding.MGF1(algorithm=hashes.SHA256()),
+    #         algorithm=hashes.SHA256(),
+    #         label=None
+    #     )
+    # )
 
-    return jsonify({"encrypted_key": "some_encrypted_key"})
+    return {'encrypted_key': decrypted_key}
 
 @app.route('/test', methods=['POST'])
 def test_endpoint():
