6770361e16a5abede19fe8c94276b59b5ef26f8c
Yan Michalevsky <yan@cryptosat.io>
2023-09-24 03:45:42 +0000

    Updated server

--- serve.py
+++ serve.py
@@ -6,6 +6,8 @@ from cryptography.hazmat.primitives import serialization, hashes, asymmetric
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import rsa
+import json
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -19,6 +21,21 @@ PRIVATE_KEY = rsa.generate_private_key(
 
 # Mock database for the addresses and their associated details
 KEYS_AND_GUARDIANS = {}
+
+STORAGE_FILE = 'cryptosat_state.json'
+
+def save_state():
+    with open(STORAGE_FILE, 'w') as f:
+        json.dump(KEYS_AND_GUARDIANS, f)
+
+def load_state():
+    global KEYS_AND_GUARDIANS
+    if os.path.exists(STORAGE_FILE):
+        # Reading data back
+        with open(STORAGE_FILE, 'r') as f:
+            KEYS_AND_GUARDIANS = json.load(f)
+        
+        # Now my_dict is a dictionary containing the data from the JSON file
 
 @app.route('/', methods=['GET'])
 def read_root():
@@ -38,6 +55,7 @@ def store_backup_key():
 
     print('State of key storage:')
     print(KEYS_AND_GUARDIANS)
+    save_state()
 
     return jsonify({"status": "Backup key, address, and guardians stored successfully"})
 
@@ -57,50 +75,59 @@ def guardian_approve():
     data_hash = hashlib.sha256(combined_data.encode()).digest()
 
     # Load the guardian's provided public key
-    public_key = serialization.load_pem_public_key(data['guardian_public_key'].encode(), 
-                                                   backend=default_backend())
+    # public_key = serialization.load_pem_public_key(data['guardian_public_key'].encode(), 
+    #                                                backend=default_backend())
+    public_key = data['guardian_public_key']
 
     # Verify the signature
-    try:
-        public_key.verify(
-            bytes.fromhex(data['signed_hash']),
-            data_hash,
-            padding.PKCS1v15(),
-            hashes.SHA256()
-        )
-    except:
-        return {"status": "Signature verification failed"}, 400
+    # try:
+    #     public_key.verify(
+    #         bytes.fromhex(data['signed_hash']),
+    #         data_hash,
+    #         padding.PKCS1v15(),
+    #         hashes.SHA256()
+    #     )
+    # except:
+    #     return {"status": "Signature verification failed"}, 400
 
     # Assuming the signature is valid, do further processing here, for example:
-    entry['approvals'] += 1
+    # For now, we don't check that the approvals are coming from distinct guardians
+    # This check MUST be added
+    KEYS_AND_GUARDIANS[old_loser_address]['approvals'] += 1
+    save_state()
 
-    return {"status": "Approved"}
+    return {"status": "Approved"}, 200
 
 @app.route('/restorekey', methods=['POST'])
 def restore_key():
-    GUARDIAN_THRESHOLD = 3
+    GUARDIAN_THRESHOLD = 2
+    print('restorekey')
     
     # Replace this with how you're getting the old_user_address
-    old_user_address = request.json
+    old_loser_address = request.json['old_loser_address']
+
+    entry = KEYS_AND_GUARDIANS.get(old_loser_address)
     
-    entry = KEYS_AND_GUARDIANS.get(old_user_address)
+    if not entry:
+        abort(400, "Address not found")
     
-    if not entry or entry["approvals"] < GUARDIAN_THRESHOLD:
-        abort(400, "Insufficient approvals or address not found")
+    if entry["approvals"] < GUARDIAN_THRESHOLD:
+        abort(400, "Insufficient approvals")
 
     # Decrypt the backup key
-    decrypted_key = PRIVATE_KEY.decrypt(
-        entry["enc_backup_key"].encode(),
-        padding.OAEP(
-            mgf=padding.MGF1(algorithm=hashes.SHA256()),
-            algorithm=hashes.SHA256(),
-            label=None
-        )
-    )
+    # decrypted_key = PRIVATE_KEY.decrypt(
+    #     entry["enc_backup_key"].encode(),
+    #     padding.OAEP(
+    #         mgf=padding.MGF1(algorithm=hashes.SHA256()),
+    #         algorithm=hashes.SHA256(),
+    #         label=None
+    #     )
+    # )
+    decrypted_key = entry['enc_backup_key']
     
     # Encrypt the decrypted key with the provided RSA public key (new_address)
-    new_address_public_key = serialization.load_pem_public_key(entry["new_address"].encode(), 
-                                                               backend=default_backend())
+    # new_address_public_key = serialization.load_pem_public_key(entry["new_address"].encode(), 
+    #                                                            backend=default_backend())
     # encrypted_key = new_address_public_key.encrypt(
     #     decrypted_key,
     #     padding.OAEP(
@@ -110,7 +137,7 @@ def restore_key():
     #     )
     # )
 
-    return {'encrypted_key': decrypted_key}
+    return {'encrypted_key': decrypted_key}, 200
 
 @app.route('/test', methods=['POST'])
 def test_endpoint():
@@ -118,4 +145,6 @@ def test_endpoint():
     return jsonify({"received_data": input_data})
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=9000)
+    load_state()
+    print(KEYS_AND_GUARDIANS)
+    app.run(debug=True, host='0.0.0.0', port=9000)
